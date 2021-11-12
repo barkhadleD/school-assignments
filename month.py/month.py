b0a8acdfbbafdc7f1005c8694420ea2ba021b499
barkhadleD <93361722+barkhadleD@users.noreply.github.com>
2021-11-12 14:44:48 -0500

    did something

---
+++ month.py/month.py
@@ -0,0 +1,51 @@
+print('Hello, what is your name?')
+Name = input().capitalize()
+print(f'''{Name} would you like to know how many days are in a specific month
+of the year? Please answer with yes or no.''')
+yes = input()
+
+
+if yes == ('yes'):
+    print(f'ok {Name} please enter a month of the year.')
+    month = input().capitalize()
+
+    if month == ('January'):
+        print (f'there are 31 days in the month of {month}')
+        
+    elif month == ('march'):
+        print (f'there are 31 days in the month of {month}')
+        
+    elif month == ('may'):
+        print (f'there are 31 days in the month of {month}')
+        
+    elif month == ('July'):
+        print (f'there are 31 days in the month of {month}')
+        
+    elif month == ('august'):
+        print (f'there are 31 days in the month of {month}')
+        
+    elif month == ('october'):
+        print (f'there are 31 days in the month of {month}')
+        
+    elif month == ('december'):
+        print (f'there are 31 days in the month of {month}')
+        
+    
+    elif month == ('april'):
+        print(f'there are 31 days in the month of {month}')
+        
+    elif month == ('june'):
+        print(f'there are 31 days in the month of {month}')
+        
+    elif month == ('septenber'):
+        print(f'there are 31 days in the month of {month}')
+        
+    elif month == ('november'):
+        print(f'there are 31 days in the month of {month}')
+
+        
+    elif month == ('febuary'):
+        print (f'there are 31 days in the month of {month}')
+    
+print(f'Ok {Name} thank you for your time.')
+
--- summer-winter/summer-winter.py
+++ summer-winter/summer-winter.py
@@ -22,7 +22,7 @@ if season == ('summer'):
         print('Me either. You will never hear me complain')
         print ('about the weather being too hot in the summer time.')
 
-    if hot == ('yes'):
+    elif hot == ('yes'):
         print('''I find that interesting. you said that you find summer
         to be hot but yet you also said that summer was your favourite 
         time of year.''')
@@ -37,5 +37,47 @@ if season == ('winter'):
         print ('I think you are crazy. I don\'t know how stay warm')
         print()
         print(f'it\'s been nice chatting with you {Name}')
-        print('I hope we get the chance to chat again soon, ')
-print(f'I must go now {Name}. Goodbye')
+        print('I hope we get the chance to chat again soon, ')  
+          
+    elif cold == ('yes'):
+        print('I agree winter is really cold that is why i bought a jacket.')
+        print()
+        print(f'Thank you {Name} for talking to me')
+        print('We should talk more next time')
+
+#------------------------------------------------------------------------------
+if season == ('Summer'):
+    print('I like summer the best as well')
+    print('Do you ever find that summer is too hot')
+    print('Please answer yes or no')
+    hot = input ()
+
+    if hot == ('no'):
+        print('Me either. You will never hear me complain')
+        print ('about the weather being too hot in the summer time.')
+
+    elif hot == ('yes'):
+        print('''I find that interesting. you said that you find summer
+        to be hot but yet you also said that summer was your favourite 
+        time of year.''')
+
+
+if season == ('Winter'):
+    print('Don\'t you find winter too cold?')
+    print('Please answer yes or no.')
+    cold = input()
+
+    if cold == ('no'):
+        print ('I think you are crazy. I don\'t know how stay warm')
+        print()
+        print(f'it\'s been nice chatting with you {Name}')
+        print('I hope we get the chance to chat again soon, ')  
+          
+    elif cold == ('yes'):
+        print('I agree winter is really cold that is why i bought a jacket.')
+        print()
+        print(f'Thank you {Name} for talking to me')
+        print('We should talk more next time')
+
+print(f'I must go now {Name}. Goodbye.')
+# to fix the cap problem maybe you should .capitalize()
