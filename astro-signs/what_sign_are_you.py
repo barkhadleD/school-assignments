d947ced6681e5287c9cd136be76b8e885552d750
barkhadleD <93361722+barkhadleD@users.noreply.github.com>
2021-12-07 14:36:22 -0500

    finnished a assignment

--- astro-signs/what_sign_are_you.py
+++ astro-signs/what_sign_are_you.py
@@ -1,144 +1,175 @@
-#NOTES:
-#Dont forget to add the else statement if for example the user spell the       
-#month wrong or adds any value that is not added in the code.
-
-
-
-print('Hello my name is Com Puter, what is you name?.')
+print('Hello my name is Com Puter, what is you name?')
 name = input().capitalize()
 print()
 print(f'{name} would you like to know your astrological sign?')
 print('Please answer with yes or no.')
-yes = input()
-if yes == ('yes'):
-    print(f'Ok {name} please enter your birth month')
+yes = input().capitalize()
+if yes == ('Yes'):
+    print(f'Ok {name} please enter your birth month.')
     birthMonth = input().capitalize()
 
     
     if birthMonth == ('January'):
-        print('''which day in Janurary where you born? please enter only the
-number for example if you where born on january 18 please enter only 18. ''')
+        print('''Which day in Janurary where you born? Please enter only the
+number for example if you where born on January 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 19:
-            print(f'{name}, Your astrological sign is Capiricorn.')
-        elif birthDay > 19 :
-            print(f'{name}, Your astrological sign is Aquarius.')
-
+            print(f'{name}, your astrological sign is Capiricorn.')
+        elif birthDay > 19 and birthDay <= 31:
+            print(f'{name}, your astrological sign is Aquarius.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
             
-    if birthMonth == ('February'):
-        print('''which day in February where you born? please enter only the
+            
+    elif birthMonth == ('February'):
+        print('''Which day in February where you born? Please enter only the
 number for example if you where born on February 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 18:
-            print(f'{name}, Your astrological sign is Aquarius.')
-        elif birthDay > 18:
-            print(f'{name}, Your astrological sign is Pisces.')
-
+            print(f'{name}, your astrological sign is Aquarius.')
+        elif birthDay > 18 and birthDay <= 29 :
+            print(f'{name}, your astrological sign is Pisces.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
             
     elif birthMonth == ('March'):
-        print('''which day in March where you born? please enter only the
+        print('''Which day in March where you born? Please enter only the
 number for example if you where born on March 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 20:
-            print(f'{name}, Your astrological sign is Pisces.')
-        elif birthDay > 20:
-            print(f'{name}, Your astrological sign is Aries.')
-
+            print(f'{name}, your astrological sign is Pisces.')
+        elif birthDay > 20 and birthDay <= 31:
+            print(f'{name}, your astrological sign is Aries.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
             
     elif birthMonth == ('April'):
-        print('''which day in April where you born? please enter only the
+        print('''Which day in April where you born? Please enter only the
 number for example if you where born on April 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 20:
-            print(f'{name}, Your astrological sign is Aries.')
-        elif birthDay > 20:
-            print(f'{name}, Your astrological sign is Taurus.')
-
+            print(f'{name}, your astrological sign is Aries.')
+        elif birthDay > 20 and birthDay <= 30:
+            print(f'{name}, your astrological sign is Taurus.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
         
     elif birthMonth == ('May'):
-        print('''which day in May where you born? please enter only the
+        print('''Which day in May where you born? Please enter only the
 number for example if you where born on May 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 20:
-            print(f'{name}, Your astrological sign is Taurus.')
-        elif birthDay > 20:
-            print(f'{name}, Your astrological sign is Gemini.')
-
+            print(f'{name}, your astrological sign is Taurus.')
+        elif birthDay > 20  and birthDay <= 31:
+            print(f'{name}, your astrological sign is Gemini.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
         
     elif birthMonth == ('June'):
-        print('''which day in June where you born? please enter only the
+        print('''Which day in June where you born? Please enter only the
 number for example if you where born on June 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 21:
-            print(f'{name}, Your astrological sign is Gemini.')
-        elif birthDay > 21:
-            print(f'{name}, Your astrological sign is Cancer.')
-
+            print(f'{name}, your astrological sign is Gemini.')
+        elif birthDay > 21 and birthDay <= 30:
+            print(f'{name}, your astrological sign is Cancer.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
         
     elif birthMonth == ('July'):
-        print('''which day in July where you born? please enter only the
+        print('''Which day in July where you born? Please enter only the
 number for example if you where born on July 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 22:
-            print(f'{name}, Your astrological sign is Cancer.')
-        elif birthDay > 22:
-            print(f'{name}, Your astrological sign is Leo.')
-
+            print(f'{name}, your astrological sign is Cancer.')
+        elif birthDay > 22 and birthDay <= 31:
+            print(f'{name}, your astrological sign is Leo.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
         
     elif birthMonth == ('August'):
-        print('''which day in August where you born? please enter only the
+        print('''Which day in August where you born? Please enter only the
 number for example if you where born on August 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 22:
-            print(f'{name}, Your astrological sign is Leo.')
-        elif birthDay > 22:
-            print(f'{name}, Your astrological sign is Virgo.')
-
+            print(f'{name}, your astrological sign is Leo.')
+        elif birthDay > 22 and birthDay <= 31:
+            print(f'{name}, your astrological sign is Virgo.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
         
     elif birthMonth == ('September'):
-        print('''which day in September where you born? please enter only the
+        print('''Which day in September where you born? Please enter only the
 number for example if you where born on September 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 22:
-            print(f'{name}, Your astrological sign is Virgo.')
-        elif birthDay > 22:
-            print(f'{name}, Your astrological sign is Libra.')
-
+            print(f'{name}, your astrological sign is Virgo.')
+        elif birthDay > 22 and birthDay < 30:
+            print(f'{name}, your astrological sign is Libra.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
         
     elif birthMonth == ('October'):
-        print('''which day in October where you born? please enter only the
+        print('''Which day in October where you born? Please enter only the
 number for example if you where born on October 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 24:
-            print(f'{name}, Your astrological sign is Libra.')
-        elif birthDay > 24:
-            print(f'{name}, Your astrological sign is Scorpio.')
-
+            print(f'{name}, your astrological sign is Libra.')
+        elif birthDay > 24 and birthDay <= 31:
+            print(f'{name}, your astrological sign is Scorpio.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
         
     elif birthMonth == ('November'):
-        print('''which day in November where you born? please enter only the
+        print('''Which day in November where you born? Please enter only the
 number for example if you where born on November 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 23:
-            print(f'{name}, Your astrological sign is Scorpio.')
-        elif birthDay > 23:
-            print(f'{name}, Your astrological sign is Sagittarius.')
-
+            print(f'{name}, your astrological sign is Scorpio.')
+        elif birthDay > 23 and birthDay <= 30:
+            print(f'{name}, your astrological sign is Sagittarius.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+            
             
     elif birthMonth == ('December'):
-        print('''which day in December where you born? please enter only the
+        print('''Which day in December where you born? Please enter only the
 number for example if you where born on December 18 please enter only 18. ''')
         birthDay = int(input())
         if birthDay <= 21:
-            print(f'{name}, Your astrological sign is Sagittarius.')
-        elif birthDay > 21:
-            print(f'{name}, Your astrological sign is Capricorn.')
-
+            print(f'{name}, your astrological sign is Sagittarius.')
+        elif birthDay > 21 and birthDay <= 31:
+            print(f'{name}, your astrological sign is Capricorn.')
+        else:
+            print(f'''Sorry {name} but there is not {birthDay} Days in
+{birthMonth}.''')
+        
             
     else:
-        print('L')
+        print(f'''Sorry {name} but {birthMonth} is not a month in the the Gregorian
+calendar.''')
 
         
-else:
-    print(f'''Ok {name} if you ever want to know your astrological sign let me
+elif yes == ('no'):
+        print(f'''Ok {name} if you ever want to know your astrological sign let me
 know.''')
